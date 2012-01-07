8487e118ab4cf761028e028c87c0d3fee917d329
John Fletcher <john@saltwebsites.com>
2012-01-08 00:31:08 +0100

    Default Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Netball::Application.config.secret_token = '37472fb6b8bed81d96940086a9ba9addb7e8bda70e6f7634432a8acc774bb06012e35e824c62f6fe4c5667e55f227049abdb0d8824b742ea7a817453f9bfe400'
